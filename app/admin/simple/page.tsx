8a601dda47c7d0d79201b035a1d4a088994663df
bigbaby-web <bigbaby.xyz@gmail.com>
2026-03-13 23:48:15 +0300

    Add simple test admin page

---
+++ app/admin/simple/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function SimpleAdminPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Тестовая админ-панель</h1>
+      <p>Если ты это видишь, значит админка доступна!</p>
+    </div>
+  )
+}
