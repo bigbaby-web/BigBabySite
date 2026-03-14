382be173e9cd0899020ba6d8a24ff0514531734b
bigbaby-web <bigbaby.xyz@gmail.com>
2026-03-14 09:35:39 +0300

    Add test page

---
+++ app/admin/test/page.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+export default function AdminTestPage() {
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>Тестовая страница</h1>
+      <p>Если ты это видишь, значит страницы грузятся!</p>
+    </div>
+  )
+}
